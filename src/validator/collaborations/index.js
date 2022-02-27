a0e5eb08c598be3adf4f174aaa83aa5ab7868af4
Musthafa Faishal <musthafafaishal@gmail.com>
2022-02-27 13:47:09 +0700

    Creating collaborations plugin

--- src/validator/collaborations/index.js
+++ src/validator/collaborations/index.js
@@ -3,8 +3,7 @@ const { CollaborationPayloadSchema } = require('./schema');
 
 const CollaborationsValidator = {
   validateCollaborationPayload: (payload) => {
-    const validationResult =
-      CollaborationPayloadSchema.validateCollaborationPayload(payload);
+    const validationResult = CollaborationPayloadSchema.validate(payload);
 
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
